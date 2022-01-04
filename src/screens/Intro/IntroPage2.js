65063356b0018a1aef9a2dcf24165d406efe94c4
Anima-Sola <animasola@yandex.ru>
2022-01-04 22:43:38 +0300

    Intro

--- src/screens/Intro/IntroPage2.js
+++ src/screens/Intro/IntroPage2.js
@@ -1,57 +1,87 @@
-import React, { useState, useRef, useEffect, useCallback } from 'react';
-import { StyleSheet, Text, View, Image, Animated } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 import { Button } from 'react-native-elements';
 import { THEME } from '../../styles/theme';
+import Carousel from 'react-native-snap-carousel';
+
+import FlatImage from "../../assets/images/PosessionList/flat.png";
+import CarImage from "../../assets/images/PosessionList/car.png";
+import VillaImage from "../../assets/images/PosessionList/villa.png";
+import YachtImage from "../../assets/images/PosessionList/yacht.png";
+import PlaneImage from "../../assets/images/PosessionList/plane.png";
+
+class MyCarousel extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        activeIndex:0,
+        carouselItems: [
+        {
+            image: FlatImage,
+            text: "КВАРТИРУ",
+        },
+        {
+            image: CarImage,
+            text: "МАШИНУ",
+        },
+        {
+            image: VillaImage,
+            text: "ВИЛЛУ",
+        },
+        {
+            image: YachtImage,
+            text: "ЯХТУ",
+        },
+        {
+            image: PlaneImage,
+            text: "САМОЛЕТ",
+        },
+        ]
+    }
+    }
+
+    _renderItem({item,index}){
+        return (
+            <View style={ styles.carouselItem }>
+                <View style={ styles.carouselItemImage }>
+                    <Image style={ styles.itemImage } resizeMode='center' source={ item.image } />
+                </View>
+                <View style={ styles.carouselItemText }>
+                    <Text style={ styles.itemText }>{item.text}</Text>
+                </View>
+            </View>
+        )
+    }
+
+    render() {
+        return (
+            <Carousel
+                autoplay={true}
+                autoplayInterval={3000}
+                autoplayDelay={100}
+                scrollEnabled={false}
+                loop={true}
+                layout={"default"}
+                ref={ref => this.carousel = ref}
+                data={this.state.carouselItems}
+                sliderWidth={350}
+                itemWidth={350}
+                renderItem={this._renderItem}
+                onSnapToItem = { index => this.setState({activeIndex:index}) } 
+            />
+        );
+    }
+}
 
 export const IntroPage2 = () => {
     return (
         <View style={ styles.container }>
-            <View style={ styles.listContainer }>
-                <View style={ styles.items }>
-                    <View style={ styles.header }>
-                        <Text style={ styles.headerText }>Можете даже представить, что вы имеете:</Text>
-                    </View>
-                    <View style={ styles.item }>
-                        <View>
-                            <Text style={ styles.text }>Квартиру -</Text>
-                        </View>
-                        <View>
-                            <Image style={ styles.image } resizeMode='center' source={ require('../../assets/images/PosessionList/flat.png') } />
-                        </View>
-                    </View>
-                    <View style={ styles.item }>
-                        <View>
-                            <Image style={ styles.image } resizeMode='center' source={ require('../../assets/images/PosessionList/car.png') } />
-                        </View>
-                        <View>
-                            <Text style={ styles.text }>- Машину</Text>
-                        </View>
-                    </View>
-                    <View style={ styles.item }>
-                        <View>
-                            <Text style={ styles.text }>Виллу -</Text>
-                        </View>
-                        <View>
-                            <Image style={ styles.image } resizeMode='center' source={ require('../../assets/images/PosessionList/villa.png') } />
-                        </View>
-                    </View>
-                    <View style={ styles.item }>
-                        <View>
-                            <Image style={ styles.image } resizeMode='center' source={ require('../../assets/images/PosessionList/yacht.png') } />
-                        </View>
-                        <View>
-                            <Text style={ styles.text }>- Яхту</Text>
-                        </View>
-                    </View>
-                    <View style={ styles.item }>
-                        <View>
-                            <Text style={ styles.text }>Самолет -</Text>
-                        </View>
-                        <View>
-                            <Image style={ styles.image } resizeMode='center' source={ require('../../assets/images/PosessionList/plane.png') } />
-                        </View>
-                    </View>
-                </View>
+            <View style={ styles.header }>
+                <Text style={ styles.headerText }>Представьте, что вы имеете:</Text>
+            </View>
+            <View style={ styles.carousel }>
+                <MyCarousel />
             </View>
             <View style={ styles.swipeContainer }>
                 <Image resizeMode='center' source={ require('../../assets/images/swipe.gif') } />
@@ -72,47 +102,49 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         paddingTop: THEME.STATUSBAR_HEIGHT,
     },
-    listContainer: {
-        flex: 0.72,
+    header: {
+        flex: 0.10,
         /*borderColor: "#fff",
         borderStyle: "solid",
-        borderWidth: 1*/
-    },
-    items: {
-        flex: 1,
-        justifyContent: 'flex-end'
-    },
-    header: {
-        flex: 0.16
-    },
-    item: {
-        flexDirection: 'row',
-        flex: 0.16,
-        alignItems: 'center',
-        justifyContent:'space-around'
+        borderWidth: 1,*/
+        justifyContent: "center"
     },
     headerText: {
         color: THEME.TEXT_COLOR,
         fontFamily: 'nunito-light',
-        fontSize: THEME.FONT25,
-        textAlign: 'center',
-        justifyContent: "center",
+        fontSize: THEME.FONT20,
+        textAlign: 'center'
     },
-    text: {
-        color: THEME.TEXT_COLOR,
-        fontFamily: 'nunito-extralight',
-        fontSize: THEME.FONT25,
-        textAlign: 'center',
-        justifyContent: "center",
+    carousel: {
+        flex: 0.62,
     },
-    image: {
-        flex: 1,
-        width: 0.35 * THEME.SCREEN_WIDTH,
-        height: 0.35 * THEME.SCREEN_WIDTH * THEME.SCREEN_ASPECT_RATIO,
+    carouselItem: {
+        borderRadius: 5,
+        backgroundColor: THEME.MAIN_BACKGROUND_COLOR,
+        borderColor: "#000",
+        borderStyle: "solid",
+        borderWidth: 1,
+    },
+    carouselItemImage: {
+        alignItems:'center',
+        /*borderColor: "#fff",
+        borderStyle: "solid",
+        borderWidth: 1*/
+    },
+    itemImage: {
+        width: 0.8 * THEME.SCREEN_WIDTH,
+        height: 0.55 * THEME.SCREEN_WIDTH * THEME.SCREEN_ASPECT_RATIO,
         justifyContent: 'center',
-        //borderColor: "#fff",
-        //borderStyle: "solid",
-        //borderWidth: 1
+    },
+    carouselItemText: {
+        backgroundColor: '#000',
+        padding: 10
+    },
+    itemText: {
+        color: '#fff',
+        fontFamily: 'nunito-semibold',
+        fontSize: THEME.FONT20,
+        textAlign: 'center'
     },
     swipeContainer: {
         flex: 0.10,
